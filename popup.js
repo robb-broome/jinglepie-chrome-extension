3cab5665af8d4e401bec4781c48cb27bf1736ae9
robb-broome <robb@teamvoice.com>
2015-08-24 12:53:28 -0500

    submit only. Needs login and either an api or a #new to work.

---
+++ popup.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var checkPageButton = document.getElementById('saveToJpie');
+  checkPageButton.addEventListener('click', function() {
+
+    chrome.tabs.getSelected(null, function(tab) {
+      d = document;
+
+      var f = d.createElement('form');
+      f.action = 'http://www.jinglepie.com/nodes';
+      f.method = 'post';
+      var i = d.createElement('input');
+      i.type = 'text';
+      i.name = 'link';
+      i.value = tab.url;
+      f.appendChild(i);
+      var name = d.createElement('input');
+      name.type = 'text';
+      name.name = 'name';
+      name.value = 'fromTheJpieExt';
+      f.appendChild(name);
+      d.body.appendChild(f);
+      f.submit();
+    });
+  }, false);
+}, false);
